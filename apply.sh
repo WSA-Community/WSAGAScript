7666d0360569ac765f57a1b79b435d3b694117df
Christopher Mason <3318784+nihil-2019@users.noreply.github.com>
2021-10-25 04:34:11 -0400

    remove old overlay dirs to suppress errors`

--- apply.sh
+++ apply.sh
@@ -16,7 +16,6 @@ rm -Rf $InstallDir/priv-app/PackageInstaller/
 echo "Copying GApps files to system..."
 cp -f -a $GAppsOutputFolder/app/* $InstallDir/app
 cp -f -a $GAppsOutputFolder/etc/* $InstallDir/etc
-cp -f -a $GAppsOutputFolder/overlay/* $InstallDir/overlay
 cp -f -a $GAppsOutputFolder/priv-app/* $InstallDir/priv-app
 cp -f -a $GAppsOutputFolder/framework/* $InstallDir/framework
 cp -fra  $GAppsRoot/product_output/* $MountPointProduct/
@@ -24,7 +23,6 @@ cp -fra  $GAppsRoot/product_output/* $MountPointProduct/
 echo "Applying root file ownership"
 find $InstallDir/app -exec chown root:root {} &>/dev/null \;
 find $InstallDir/etc -exec chown root:root {} &>/dev/null \;
-find $InstallDir/overlay -exec chown root:root {} &>/dev/null \;
 find $InstallDir/priv-app -exec chown root:root {} &>/dev/null \;
 find $InstallDir/framework -exec chown root:root {} &>/dev/null \;
 find $InstallDir/lib -exec chown root:root {} &>/dev/null \;
@@ -34,7 +32,6 @@ find $MountPointProduct/overlay -exec chown root:root {} &>/dev/null \;
 echo "Setting directory permissions"
 find $InstallDir/app -type d -exec chmod 755 {} \;
 find $InstallDir/etc -type d -exec chmod 755 {} \;
-find $InstallDir/overlay -type d -exec chmod 755 {} \;
 find $InstallDir/priv-app -type d -exec chmod 755 {} \;
 find $InstallDir/framework -type d -exec chmod 755 {} \;
 find $InstallDir/lib -type d -exec chmod 755 {} \;
@@ -43,7 +40,6 @@ find $MountPointProduct/overlay -type d -exec chmod 755 {} \;
 
 echo "Setting file permissions"
 find $InstallDir/app -type f -exec chmod 644 {} \;
-find $InstallDir/overlay -type f -exec chmod 644 {} \;
 find $InstallDir/priv-app -type f -exec chmod 644 {} \;
 find $InstallDir/framework -type f -exec chmod 644 {} \;
 find $InstallDir/lib -type f -exec chmod 644 {} \;
@@ -56,7 +52,6 @@ find $MountPointProduct/overlay -type f -exec chmod 644 {} \;
 
 echo "Applying SELinux security contexts to directories"
 find $InstallDir/app -type d -exec chcon --reference=$InstallDir/app {} \;
-find $InstallDir/overlay -type d -exec chcon --reference=$InstallDir/overlay {} \;
 find $InstallDir/priv-app -type d -exec chcon --reference=$InstallDir/priv-app {} \;
 find $InstallDir/framework -type d -exec chcon --reference=$InstallDir/framework {} \;
 find $InstallDir/lib -type d -exec chcon --reference=$InstallDir/lib {} \;
@@ -69,7 +64,6 @@ find $MountPointProduct/overlay -type d -exec chcon --reference=$MountPointVendo
 
 echo "Applying SELinux security contexts to files"
 find $InstallDir/framework -type f -exec chcon --reference=$InstallDir/framework/ext.jar {} \;
-find $InstallDir/overlay -type f -exec chcon --reference=$InstallDir/app/CertInstaller/CertInstaller.apk {} \;
 find $InstallDir/app -type f -exec chcon --reference=$InstallDir/app/CertInstaller/CertInstaller.apk {} \;
 find $InstallDir/priv-app -type f -exec chcon --reference=$InstallDir/priv-app/Shell/Shell.apk {} \;
 find $InstallDir/lib -type f -exec chcon --reference=$InstallDir/lib/libcap.so {} \;
