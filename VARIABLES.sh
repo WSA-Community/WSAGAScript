c0cb882870fee93bb493c9906c38c5eaec626970
cupang-afk <pixelview56@gmail.com>
2021-10-26 05:24:40 +0700

    Added function get current directory for $Root variable
    and added get architecture from wsl architecture function

--- VARIABLES.sh
+++ VARIABLES.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 # Modify your variables here to corretly reference your directory and subdir
-Root="/mnt/c/GAppsWSA"
+# get current dir of this script
+Root="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Your Windows architecture, x64 or arm64
-Architecture="x64"
+# get architecture based on wsl architecture 
+# its always same as windows afaik
+Architecture=$(uname -m)
+if [ $Architecture == 'x86_64' ]; then
+    Architecture="x64"
+elif [ $Architecture == 'arm64' || 
+$Architecture == 'aarch64' ]; then
+    Architecture="ARM64"
+else 
+    echo "is your cpu 64 or arm64 ??"
+    exit
+fi
 
 MiscRoot="$Root/misc"
 PropRoot="$MiscRoot/prop/$Architecture"
