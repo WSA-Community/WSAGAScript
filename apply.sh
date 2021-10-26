6f7320faf82cc4ef580faabc281ff8e4489d9576
Christopher Mason <3318784+nihil-2019@users.noreply.github.com>
2021-10-26 05:00:14 -0400

    aosp sound picker -> google sounds
    as with prevous commits, this is yet
    another crusty old aosp app being
    replaced with its modern google
    equivalent.

--- apply.sh
+++ apply.sh
@@ -15,6 +15,7 @@ rm -Rf $InstallDir/apex/com.android.extservices/
 rm -Rf $InstallDir/app/DocumentsUI/
 rm -Rf $InstallDir/app/ExtShared/
 rm -Rf $InstallDir/priv-app/PackageInstaller/
+rm -Rf $InstallDir/priv-app/SoundPicker/
 rm -Rf $MountPointProduct/app/Music/
 rm -Rf $MountPointProduct/priv-app/Contacts/
 
