00f46e8b63fb3040cecf36eb49af8008d7730e1e
Christopher Mason <3318784+nihil-2019@users.noreply.github.com>
2021-10-26 04:53:25 -0400

    aosp music -> youtube music
    as with prevous commits, this is yet
    another crusty old aosp app being
    replaced with its modern google
    equivalent.

--- apply.sh
+++ apply.sh
@@ -15,6 +15,7 @@ rm -Rf $InstallDir/apex/com.android.extservices/
 rm -Rf $InstallDir/app/DocumentsUI/
 rm -Rf $InstallDir/app/ExtShared/
 rm -Rf $InstallDir/priv-app/PackageInstaller/
+rm -Rf $MountPointProduct/app/Music/
 rm -Rf $MountPointProduct/priv-app/Contacts/
 
 echo "Copying GApps files to system..."
