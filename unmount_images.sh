a4c2692dd0f4350a8c0c323c4fce6df464945508
Kiruya Momochi <65301509+KiruyaMomochi@users.noreply.github.com>
2021-10-29 12:33:30 +0900

    Shrink image after unmount

--- unmount_images.sh
+++ unmount_images.sh
@@ -14,4 +14,28 @@ umount $MountPointSystem
 echo "Unmounting vendor.img"
 umount $MountPointVendor
 
+echo "chk product.img"
+e2fsck -f $ImagesRoot/product.img
+
+echo "Resizing product.img"
+resize2fs -M $ImagesRoot/product.img
+
+echo "chk system.img"
+e2fsck -f $ImagesRoot/system.img
+
+echo "Resizing system.img"
+resize2fs -M $ImagesRoot/system.img
+
+echo "chk system_ext.img"
+e2fsck -f $ImagesRoot/system_ext.img
+
+echo "Resizing system_ext.img"
+resize2fs -M $ImagesRoot/system_ext.img
+
+echo "chk vendor.img"
+e2fsck -f $ImagesRoot/vendor.img
+
+echo "Resizing vendor.img"
+resize2fs -M $ImagesRoot/vendor.img
+
 echo "!! Unmounting completed !!"
