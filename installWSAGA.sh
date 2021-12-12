19ec457709be8da093a2310e2b106ac25aa11f00
Oliver Großkloß <oliver.grosskloss+gh@gmail.com>
2021-12-12 14:18:25 +0100

    improved installation process, folders, cleanup and spelling

--- installWSAGA.sh
+++ installWSAGA.sh
@@ -2,18 +2,18 @@ LINK='\033[1;34m' #blue
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 echo "updating..."
-sudo apt update
-sudo apt install unzip lzip git
+sudo apt-get update -qq
+sudo apt install unzip lzip git -qq
 
-while ! find MicrosoftCorporationII.WindowsSubsystemForAndroid*.msixbundle 1> /dev/null 2>&1
+while ! find MicrosoftCorporationII.WindowsSubsystemForAndroid*.Msixbundle 1> /dev/null 2>&1
 do
-  echo -e "\n\msixbundle not found - download from: ${LINK}https://store.rg-adguard.net/${NC} (Ctrl+Click)\nProductId: 9P3395VX91NR, Ring: SLOW\n\nPress enter once downloaded to current folder" ; read -p "($PWD)"
+  echo -e "\n\Msixbundle not found - download from: ${LINK}https://store.rg-adguard.net/${NC} (Ctrl+Click)\nProductId: 9P3395VX91NR, Ring: SLOW\n\nPress enter once ${YELLOW}downloaded to current folder${NC}:" ; read -p "($PWD)"
 done
-echo msixbundle found!
+echo Msixbundle found!
 
-while ! find open_gapps*pico*.zip 1> /dev/null 2>&1
+while ! find open_gapps*.zip 1> /dev/null 2>&1
 do
-  echo -e "\n\nGApps not found - download from: ${LINK}https://opengapps.org/${NC} (Ctrl+Click)\nPlatform: x86_64 if Windows architecture is x64, otherwise choose ARM64\nAndroid: 11 and Variant: Pico on\n\nPress enter once downloaded to current folder" ; read -p "($PWD)"
+  echo -e "\n\nGApps not found - download from: ${LINK}https://opengapps.org/${NC} (Ctrl+Click)\nPlatform: x86_64 if Windows architecture is x64, otherwise choose ARM64\nAndroid: 11 and Variant: Pico (or another)\n\nPress enter once ${YELLOW}downloaded to current folder${NC}" ; read -p "($PWD)"
 done
 echo GApps found!
 
@@ -34,7 +34,7 @@ while true; do
 			rm WSAGAScript/#MSIX/ARM64.msix;
 			break;;
         [Xx]* )
-			echo "extracting x64 verison, please wait...";
+			echo "extracting x64 version, please wait...";
 			unzip -p MicrosoftCorporationII.WindowsSubsystemForAndroid* *x64_Release-Nightly.msix > WSAGAScript/#MSIX/x64.msix
 			unzip WSAGAScript/#MSIX/x64.msix -d WSAGAScript/#MSIX/
 			rm WSAGAScript/#MSIX/x64.msix;
@@ -60,14 +60,21 @@ sudo ./extend_and_mount_images.sh
 sudo ./apply.sh
 sudo ./unmount_images.sh
 
-echo "moving edited images to intall location"
+echo "moving edited images to install location"
 mv ./#IMAGES/*.img ./#MSIX/
+
+
+echo "moving install folder to C:"
+mkdir /mnt/c/WSA
+mv ./#MSIX/* /mnt/c/WSA
+
 
 echo -e "${YELLOW}Nearly done:${NC}"
 echo "Enable developer mode in windows settings."
+echo "(Settings -> Privacy&security -> For developers)"
 echo "Uninstall any other installed versions of WSA"
 echo "Open Windows PowerShell (not PowerShell) as admin and run:"
-echo -e "${LINK}Add-AppxPackage -Register $(wslpath -w \#MSIX)\\AppxManifest.xml${NC}\n"
+echo -e "${LINK}Add-AppxPackage -Register C:\\WSA\\AppxManifest.xml${NC}\n"
 read -n 1 -s -r -p "Press any key to continue"
 echo
 echo
@@ -82,5 +89,11 @@ echo
 echo
 
 echo "Everyting should be installed now"
-echo "Remaining Folder/Files: WSAGAScript, MicrosoftCorporation...msixbundle, open_gapps...zip, intallWSAGA,sh"
-echo "delete them manually or keep if something breaks"
+echo "Remaining Folder/Files: WSAGAScript, MicrosoftCorporation...Msixbundle, open_gapps...zip, intallWSAGA.sh"
+read -p "Press Enter to delete or press CTRL+C to stop here"
+cd ..
+rm -rf WSAGAScript
+rm MicrosoftCorporationII.WindowsSubsystemForAndroid*.Msixbundle
+rm open_gapps*.zip
+rm installWSAGA.sh
+echo "Cleanup comlete"
